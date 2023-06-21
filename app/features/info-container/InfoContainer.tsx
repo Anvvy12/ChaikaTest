a115377685b494c2b33638230dd2ef553ccd1398
Bogdan <romanbs.wo@gmail.com>
2023-06-22 01:24:53 +0300

    s

--- app/features/info-container/InfoContainer.tsx
+++ app/features/info-container/InfoContainer.tsx
@@ -11,12 +11,23 @@ const InfoContainer: React.FC = () => {
           <span className="card-description">$1,482.70 Available</span>
         </div>
         <div className=" sup-block">
-          <h5 className="card-head points">Deally Points</h5>
+          <h5 className="card-head points">Dailly Points</h5>
           <span className="card-description ">456K</span>
         </div>
       </div>
       <div className="right-col">
-        <div className="block"></div>
+        <div className="block">
+          <div className="block-head">
+            <span className="block-head">No Payment Due</span>
+            <br />
+            <span className="block-description">
+              You`ve paid your September balance.
+            </span>
+          </div>
+          <div className="block-check">
+            <div className="block-circle"></div>
+          </div>
+        </div>
       </div>
     </article>
   );
